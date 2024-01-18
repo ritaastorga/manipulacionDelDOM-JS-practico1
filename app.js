4ff3e0671511e907b1aac38335db5ab86485e038
rita <ritaastorga10@gmail.com>
2024-01-18 13:19:17 -0300

    practicando distintas formas de obtener elementos del DOM

---
+++ app.js
@@ -0,0 +1,23 @@
+// const contenedor = document.getElementById("contenedor");
+// console.log(typeof contenedor.innerHTML);
+
+// const titulo = document.getElementById("titulo");
+// console.log(titulo);
+
+// const toppings = document.getElementsByClassName("topping");
+// console.log(toppings[1]);
+
+// const misToppings = document.getElementsByTagName("li");
+// console.log(misToppings);
+
+// const toppingFondoMarron = document.getElementsByClassName("fondoMarron");
+// console.log(toppingFondoMarron);
+
+// const aceitunas = document.querySelector(".topping.fondoNaranja");
+// console.log(typeof aceitunas);
+
+// const aceitunas = document.querySelector(".topping.fondoNaranja");
+// console.log(typeof aceitunas);
+
+const primerTopping = document.querySelector(".topping");
+console.log(primerTopping);
